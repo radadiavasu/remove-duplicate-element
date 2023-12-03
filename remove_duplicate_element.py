bd4dbe4c1fdfb241c1a511d4174d565289e431a0
Vasu Radadia <114792837+radadiavasu@users.noreply.github.com>
2023-12-03 22:26:46 +0530

    Update remove_duplicate_element.py

--- remove_duplicate_element.py
+++ remove_duplicate_element.py
@@ -1,3 +1,4 @@
+# With using Built-in Methods.
 a = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
 
 first_step = set(a)
@@ -5,3 +6,12 @@ first_step = set(a)
 second_step = list(first_step)
 
 print(second_step)
+
+# With using for Loop
+
+a = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+ans = []
+for i in a:
+  if i not in ans:
+    ans.append(a)
+print(ans)
